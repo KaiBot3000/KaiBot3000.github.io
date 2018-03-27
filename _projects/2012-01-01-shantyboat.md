bfea7647b543b9db109baa33600001e89a45db5d
Kai <riselikethemoon@gmail.com>
2018-03-27 17:53:36 -0400

    event listener for toggling classes, etc

--- _projects/2012-01-01-shantyboat.md
+++ _projects/2012-01-01-shantyboat.md
@@ -10,6 +10,6 @@ pictures:
 customScripts:
 ---
 
-Starting in 2012 I spent nights and weekends building a flat-bottomed river boat in a friend's backyard, based on designs developed by people living on rivers in the early 1900's. In 2014 we launched the Dotty went on her maiden voyage, floating down the upper reaches of the Mississippi.
+Starting in 2012 I spent nights and weekends building a flat-bottomed river boat in a friend's backyard, based on designs developed by people living on rivers in the early 1900's. In 2014 we launched the Dotty on her maiden voyage, floating down the upper reaches of the Mississippi.
 
 My then-partner turned it into his masters' thesis and eventually an ongoing project, which is documented at [peoplesriverhistory.us](peoplesriverhistory.us).
--- assets/tags.js
+++ assets/tags.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', tagCloud)
 
+let tagFilter = ['all'];
+
+// start out with all
+// if you click on one, unclick all, show only that one
+// if you click on all, unclick all others and have all
+
 function tagCloud() {
     // get list of tags from posts on page
     let tags = {};
-    let tagFilter = [];
     const tagCloudDiv = document.getElementById('tagCloudDiv');
-    console.log(tagCloudDiv);
 
     Array.from(document.getElementsByClassName('tag')).forEach(function(tag) {
         let tagText = tag.innerText.trim();
@@ -16,19 +20,54 @@ function tagCloud() {
         }
     });
 
+    // add 'all' tag
+    // let allSpan = document.createElement('span');
+    // allSpan.className = 'round-full inline-block px6 pb3 mx3 my6 bg-gray-light bg-gray-light-on-hover';
+    // allSpan.innerHTML = 'all';
+    // allSpan.addEventListener('click', toggleAll);
+    // tagCloudDiv.appendChild(allSpan);
+
     // add tag cloud html to tagcloud div
     for (tag in tags) {
         let tagSpan = document.createElement('span');
         tagSpan.className = 'round-full inline-block px6 pb3 mx3 my6 bg-gray-faint bg-gray-light-on-hover';
         tagSpan.innerHTML = tag;
+        tagSpan.addEventListener('click', toggleTag);
         tagCloudDiv.appendChild(tagSpan);
     };
 
-    // when one is clicked, toggle status
-
-    // clicked tags added to filter list
-
     // filter posts by filter list
 
+}
 
+// function toggleAll(event) {
+//     event.target.classList.toggle('bg-gray-faint');
+//     event.target.classList.toggle('bg-gray-light');
+//     if (tagFilter.indexOf('all') > -1) {
+//         tagFilter = [];
+//     } else {
+//         tagFilter = ['all'];
+//     }
+//     console.log(tagFilter);
+// }
+
+function toggleTag(event) {
+    event.target.classList.toggle('bg-gray-faint');
+    event.target.classList.toggle('bg-gray-light');
+    let index = tagFilter.indexOf(event.target.innerHTML)
+    if (index > -1) {
+        tagFilter.splice(index, 1);
+    } else {
+        tagFilter.push(event.target.innerHTML);
+    }
+    console.log(tagFilter);
+
+    // call updatePosts
+}
+
+function updatePosts() {
+    // updates posts shown based on most recent tagFilter
+
+    // if the list is empty, show everything
+    // if there's something in the list, show only posts with those tags.
 }
