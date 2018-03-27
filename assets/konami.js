5c7f9fbdab90d9b894185fa36687c36357c7917f
Kai <riselikethemoon@gmail.com>
2018-03-27 16:27:01 -0400

    add actual js

---
+++ assets/konami.js
@@ -0,0 +1,12 @@
+if (window.addEventListener) {
+    const konami = '38,38,40,40,37,39,37,39,66,65';
+    let keys = [];
+
+    window.addEventListener('keydown', function(event) {
+        keys.push(event.keyCode);
+        if (keys.toString().indexOf(konami) >= 0) {
+            console.log('you made it!');
+            keys = [];
+        }
+    }, true);
+}
